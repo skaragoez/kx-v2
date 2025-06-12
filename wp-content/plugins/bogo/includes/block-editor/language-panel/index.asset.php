8666ee42a1b9a92cf05ba935abc9c382a3a8d6d4
Süleyman Karagöz <suleyman.karagoz@komoxti.com>
2025-06-12 12:28:32 +0300

    style: enhance list styles in CSS and SCSS files
    - Added new styles for unordered lists in style.css, _elements.scss, and editor-style.css, including custom markers and spacing adjustments for better readability.
    - Updated list item margins and padding to improve overall layout consistency across different sections.

---
+++ wp-content/plugins/bogo/includes/block-editor/language-panel/index.asset.php
@@ -0,0 +1,16 @@
+<?php
+
+return array(
+	'dependencies' => array(
+		'react',
+		'wp-api-fetch',
+		'wp-components',
+		'wp-data',
+		'wp-edit-post',
+		'wp-element',
+		'wp-i18n',
+		'wp-plugins',
+		'wp-url',
+	),
+	'version' => BOGO_VERSION,
+);
---
+++ wp-content/plugins/bogo/includes/block-editor/language-switcher/index.asset.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+	'dependencies' => array(
+		'react',
+		'wp-block-editor',
+		'wp-blocks',
+		'wp-components',
+		'wp-i18n',
+		'wp-primitives',
+	),
+	'version' => BOGO_VERSION,
+);
